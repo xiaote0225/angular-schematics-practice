12b1bcc1a1c3a498b63072c9f2f0bf45550acee3
wangxun <wangxun@cyai.com>
2022-08-03 18:17:18 +0800

    feat:完成第一个测试

--- hello-world/src/hello-world/index_spec.ts
+++ hello-world/src/hello-world/index_spec.ts
@@ -5,12 +5,31 @@ import * as path from 'path';
 const collectionPath = path.join(__dirname, '../collection.json');
 
 describe('hello-world', () => {
-  it('works', async () => {
-    const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = await runner
-      .runSchematicAsync('hello-world', {}, Tree.empty())
-      .toPromise();
+  const expectResult = async (fileName?:string) => {
+    const fullFileName = `/${fileName || 'hello'}`;
+    const params = fileName ? {name: fileName} : {};
+    const runner = new SchematicTestRunner('schematics',collectionPath);
+    const tree = await runner.runSchematicAsync('hello-world',params,Tree.empty()).toPromise()
+    expect(tree.files).toContain(fullFileName);
+    expect(tree.readContent(fullFileName)).toEqual('world');
+  }
 
-    expect(tree.files).toEqual([]);
+  // it('使用者没给档案名，则档案名为\'hello\'',async () => {
+  //   const runner = new SchematicTestRunner('schematics',collectionPath);
+  //   const tree = await runner.runSchematicAsync('hello-world',{},Tree.empty()).toPromise();
+  //   expect(tree.files).toContain('/hello');
+  // });
+  // it('使用者有给档案名，则档案名为使用者给的档案名',async () => {
+  //   const fileName = 'Leo';
+  //   const runner = new SchematicTestRunner('schematics',collectionPath);
+  //   const tree = await runner.runSchematicAsync('hello-world',{name:fileName},Tree.empty()).toPromise();
+  //   expect(tree.files).toContain(`/${fileName}`);
+  // });
+
+  it('使用者没给档案名，则档案名为"/hello",档案内容为world',async ()=>{
+    expectResult();
+  });
+  it('使用者没给档案名，则档案名为使用者给的档案名,档案内容为world',async ()=>{
+    expectResult('Leo');
   });
 });
